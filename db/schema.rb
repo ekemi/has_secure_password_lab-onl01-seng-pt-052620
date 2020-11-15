c545733f82e332bc1122af365ef9bd9a9be58782
edmundo papi <edmundopierre@yahoo.com>
2020-11-15 09:00:26 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201115083022) do
+ActiveRecord::Schema.define(version: 20201115085856) do
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.string   "name"
   end
 
 end
