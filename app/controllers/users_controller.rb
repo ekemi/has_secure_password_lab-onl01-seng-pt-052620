32c3aa33e3fb73e03651219fae8ec23de2cacea1
edmundo papi <edmundopierre@yahoo.com>
2020-11-15 09:06:05 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ def create
   @user = User.new(user_params)
   if @user.save
     session[:user_id] = @user.id
-    redirect_to homepage(@user)
+    redirect_to homepage_path(@user)
   else
     redirect_to '/'
   end
